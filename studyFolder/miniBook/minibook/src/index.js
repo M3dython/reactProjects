5cb0ac01ca7ffcf35128419d3ad24f4f020ac985
M3dython <samuelcbba@gmail.com>
2021-06-26 17:34:41 -0300

    minibook

--- studyFolder/miniBook/minibook/src/index.js
+++ studyFolder/miniBook/minibook/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Booklist() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return <img src='https://randomuser.me/api/portraits/men/15.jpg' alt='' />;
+};
+
+const Title = () => <h1>This is my title component</h1>;
+const Author = () => <h4>Randio Merio</h4>;
+
+ReactDOM.render(<Booklist />, document.getElementById('root'));
