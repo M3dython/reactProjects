18386a866de8ea2b525c4f1f0fb63012c289f60f
M3dython <samuelcbba@gmail.com>
2021-06-28 20:40:25 -0300

    Questions Accordion

--- projects/04-accordion/setup/src/App.js
+++ projects/04-accordion/setup/src/App.js
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  //save the array data inside questions to be updated trough setQuestions
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className='container'>
+        <h3>questions ans answers about login</h3>
+        <section className='info'>
+          {questions.map((question) => (
+            //pass the key using id and the prop question (item of questions) with the spread operator
+            <SingleQuestion key={question.id} {...question} />
+          ))}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- projects/04-accordion/setup/src/Question.js
+++ projects/04-accordion/setup/src/Question.js
@@ -1,7 +1,27 @@
 import React, { useState } from 'react';
+//icons to be used imported from the react-icon library
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ title, info }) => {
+  //use state to display or hidden info inside paragraph
+  //when useState is true the paragraph will be displayed
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        {/* onClick toggle the value of showInfo  */}
+        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+          {/* if show info is true returns + icon if its false returns - icon */}
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {
+        //if showInfo is true displays the paragraph
+        showInfo && <p>{info}</p>
+      }
+    </article>
+  );
 };
 
 export default Question;
