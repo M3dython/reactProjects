658960a0e9aea729f45d8b8180292b5db1bbb000
M3dython <samuelcbba@gmail.com>
2021-06-29 20:34:03 -0300

    updated

--- studyFolder/tutorial2/src/App.js
+++ studyFolder/tutorial2/src/App.js
@@ -18,7 +18,8 @@ import React from 'react';
 // import Setup from './tutorial/8-useContext/setup/1-context-api';
 // import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example';
 // gets the file with the name of index.js
-import Setup from './tutorial/10-prop-types/setup';
+// import Setup from './tutorial/10-prop-types/setup';
+import Setup from './tutorial/11-react-router/setup';
 
 function App() {
   return (
--- studyFolder/tutorial2/src/tutorial/11-react-router/setup/index.js
+++ studyFolder/tutorial2/src/tutorial/11-react-router/setup/index.js
@@ -1,3 +1,6 @@
+//creates a single page application without doing a full page refresh
+//react-router is a popular react library used to create spa
+//when changing version, there could be a change in syntax
 import React from 'react';
 // react router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
