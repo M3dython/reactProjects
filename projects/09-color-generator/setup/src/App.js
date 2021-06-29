4a9b654d2fd201c26d74e9ed2b0a5eee7e0ad339
M3dython <samuelcbba@gmail.com>
2021-06-29 20:32:29 -0300

    color generator

--- projects/09-color-generator/setup/src/App.js
+++ projects/09-color-generator/setup/src/App.js
@@ -1,10 +1,72 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
 
-import Values from 'values.js'
+// using external library value to generate different shades of colors
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+  //state values
+  //color input, to be mapped
+  const [color, setColor] = useState('');
+  // error if the input is incorrect
+  //boolean states are used with a function isBoolean
+  const [error, setError] = useState(false);
+  //list of colors to be displayed
+  const [list, setList] = useState(new Values('#324567').all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+    //try to get the shades of color, if there is no such color generates an error
+    try {
+      //divide 100% by 10 and returns the shades of arrays
+      let colors = new Values(color).all(10);
+      //set the shades into state colors
+      setList(colors);
+    } catch (error) {
+      // set error state to true if there is an error
+      setError(true);
+      console.log(error);
+    }
+  };
+  return (
+    //reactFragment is used, because return can only return one element in jsx
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          {/* passes the value of the input to the state color */}
+          <input
+            type='text'
+            value={color}
+            placeholder='#324567 '
+            onChange={(e) => setColor(e.target.value)}
+            //adds the class of error to the input if there is an error, the class makes the border red
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      {/* iterate over the list and display colors fetch from the api Values */}
+      <section className='colors'>
+        {list.map((color, index) => {
+          //SingleColor receives the prop colors with destructuring
+          //passes the prop hexColor with the value of the object color.hex
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              //the index is used to change the color of the hex text, once the colors start to get to dark
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
--- projects/09-color-generator/setup/src/SingleColor.js
+++ projects/09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from 'react';
+import rgbToHex from './utils';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+//receive from the props color and destructuring
+//index is generated trough map
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  // state alert to displayed once something is copied to clipboard
+  const [alert, setAlert] = useState(false);
+  //using util, convert the color rgb to an hex
+  const hex = rgbToHex(...rgb);
+  //converts the hex color fetch from the api Values to a string
+  const hexValue = `#${hexColor.toUpperCase()}`;
+  //removes the paragraph every time the alert is changed
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    //clearTimeout clears the time out so it wont accumulate with every change of alert
+    return () => clearTimeout(timeout);
+  }, [alert]);
+  //converts the rgb array into a string with commas in between
+  const bcg = rgb.join(',');
 
-export default SingleColor
+  return (
+    //if the index is bigger than ten change the color of the text to a visible one, by adding a class called
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        //copies to the clipboard, javascript function
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'> copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
